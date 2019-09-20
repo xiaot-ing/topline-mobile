a0422a3293d93b28db2f5bd8a9fdc773cbf7901f
晓婷吖 <1817631880@qq.com>
2019-09-20 10:51:37 +0800

    list组件--自带上拉加载更多---文章列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,14 +9,52 @@
     v-for="index in 8"
    :title="'标签 ' + index"
    :key="index">
-    内容 {{ index }}
+   <!-- 文章列表----自带上拉加载更多 注意嵌套关系-->
+   <van-list
+   v-model="loading"
+   :finished="finished"
+   finished-text="没有更多了"
+   @load="onLoad"
+    >
+  <van-cell
+    v-for="item in list"
+    :key="item"
+    :title="item"
+  />
+</van-list>
   </van-tab>
 </van-tabs>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
+}
+
 </script>
 
 <style>
