735e2fba32b572d037c4aef74a6b65b064a43df8
晓婷吖 <1817631880@qq.com>
2019-09-20 10:17:09 +0800

    导航头、频道列表初始化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-      首页
+      <!-- 导航头 -->
+    <van-nav-bar
+    title="黑马头条" />
+    <!-- 标签栏 频道列表 -->
+    <van-tabs>
+   <van-tab
+    v-for="index in 8"
+   :title="'标签 ' + index"
+   :key="index">
+    内容 {{ index }}
+  </van-tab>
+</van-tabs>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
