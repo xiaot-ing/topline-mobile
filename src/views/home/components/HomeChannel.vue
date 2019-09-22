077c040db3e5e1bbf3ecfaff24d2fb3dedb12a60
晓婷吖 <1817631880@qq.com>
2019-09-22 23:49:10 +0800

    创建HomeChanne组件，

---
+++ src/views/home/components/HomeChannel.vue
@@ -0,0 +1,23 @@
+<template>
+  <van-popup
+  v-model="show"
+  position="bottom"
+  :style="{ height: '90%' }"
+>
+hello world
+  </van-popup>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
