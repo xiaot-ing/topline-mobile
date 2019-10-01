b8d9aa741ff3a0025610a76e6f8f1016053f5c69
晓婷吖 <1817631880@qq.com>
2019-10-01 09:33:02 +0800

    创建搜索结果组件和简化路由传参

---
+++ src/views/search-result/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      搜索页面
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['q'],
+  created () {
+    console.log(this.q)
+  }
+}
+</script>
+
+<style>
+
+</style>
