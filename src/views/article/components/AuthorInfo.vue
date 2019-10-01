a7c4ed53945c7d9d328b11df9b09c2efd09d576d
晓婷吖 <1817631880@qq.com>
2019-10-01 21:20:47 +0800

    作者信息

---
+++ src/views/article/components/AuthorInfo.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="auth-info" v-if="article">
+    <div class="base-info">
+      <img class="avatar" :src="article.aut_photo" alt="">
+      <div>
+        <p>{{ article.aut_name }}</p>
+        <p>{{ article.pubdate | fmtDate }}</p>
+      </div>
+    </div>
+    <div>
+      <van-button
+        @click="handleFollow"
+        :type="article.is_followed ? 'default' : 'danger'"
+        :loading="false"
+      >{{ article.is_followed ? '已' : '' }}关注</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthorInfo',
+  data(){
+      return{
+          
+      }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.auth-info {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 26px;
+  .base-info {
+    display: flex;
+    align-items: center;
+  }
+  .avatar {
+    margin-right: 10px;
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+  }
+}
+</style>
